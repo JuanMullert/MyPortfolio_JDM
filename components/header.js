db6f3207a32a50908edfe258a6d5b8b3fe908390
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-03-12 13:39:43 -0500

    created structure

---
+++ components/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/Header.css';
+
+export default function header() {
+    return (
+    <div className="headerContainer">
+        <h1>Juan Mullert</h1>
+        <nav>
+        <ul>
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+            <li>Resume</li>
+        </ul>
+    </nav>
+        </div>
+        
+    )
+
+}
